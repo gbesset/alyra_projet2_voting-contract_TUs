8a9c7840561ef29654785c244b414836ae61febc
gbesset <guillaume.besset.pro@gmail.com>
2023-02-22 16:53:06 +0100

    FEAT: add some event managemet

--- test/votingTest.js
+++ test/votingTest.js
@@ -39,13 +39,15 @@ contract("Voting", (accounts) => {
 
   async function ownerAddOwnerAsVoter() {
     //Add owner as voter to access functions & check he is registered
-    await VotingInstance.addVoter(_owner, { from: _owner });
+    let tx = await VotingInstance.addVoter(_owner, { from: _owner });
+    expectEvent(tx, "VoterRegistered", { voterAddress: _owner });
     expect((await VotingInstance.getVoter(_owner, { from: _owner })).isRegistered).to.be.equal(true);
   }
 
   async function ownerAddVoter1() {
     //Add a voter
-    await VotingInstance.addVoter(_voter1, { from: _owner });
+    let tx = await VotingInstance.addVoter(_voter1, { from: _owner });
+    expectEvent(tx, "VoterRegistered", { voterAddress: _voter1 });
     expect((await VotingInstance.getVoter(_voter1, { from: _voter1 })).isRegistered).to.be.equal(true);
   }
 
@@ -143,7 +145,8 @@ contract("Voting", (accounts) => {
 
           // Add voter OK on RegisteringVoters state
           expect((await VotingInstance.getVoter(_voter1, { from: _owner })).isRegistered).to.be.equal(false);
-          await VotingInstance.addVoter(_voter1, { from: _owner });
+          let tx = await VotingInstance.addVoter(_voter1, { from: _owner });
+          expectEvent(tx, "VoterRegistered", { voterAddress: _voter1 });
           expect((await VotingInstance.getVoter(_voter1, { from: _owner })).isRegistered).to.be.equal(true);
 
           // Revert on other states
@@ -171,13 +174,15 @@ contract("Voting", (accounts) => {
           expect((await VotingInstance.getVoter(_voter1, { from: _owner })).isRegistered).to.be.equal(false);
 
           //First time
-          await VotingInstance.addVoter(_voter1, { from: _owner });
+          let tx = await VotingInstance.addVoter(_voter1, { from: _owner });
+          expectEvent(tx, "VoterRegistered", { voterAddress: _voter1 });
           expect((await VotingInstance.getVoter(_voter1, { from: _owner })).isRegistered).to.be.equal(true);
 
-          //Second time
+          //Second time error
           await expectRevert(VotingInstance.addVoter(_voter1, { from: _owner }), "Already registered");
         });
       });
+
       describe("ProposalsRegistrationStarted state", () => {
         const _proposalGenesis = "GENESIS";
         const _proposal = "new awesome propsal for voting";
@@ -200,8 +205,10 @@ contract("Voting", (accounts) => {
           expect(propGenesis.description).to.be.equal(_proposalGenesis);
           expect(propGenesis.voteCount).to.be.bignumber.equal(new BN(0));
 
-          await VotingInstance.addProposal(_proposal, { from: _voter1 });
-          await VotingInstance.addProposal(_proposal2, { from: _voter1 });
+          let tx = await VotingInstance.addProposal(_proposal, { from: _voter1 });
+          expectEvent(tx, "ProposalRegistered", { proposalId: new BN(1) });
+          tx = await VotingInstance.addProposal(_proposal2, { from: _voter1 });
+          expectEvent(tx, "ProposalRegistered", { proposalId: new BN(2) });
 
           const prop1 = await VotingInstance.getOneProposal(1, { from: _voter1 });
           const prop2 = await VotingInstance.getOneProposal(2, { from: _voter1 });
@@ -262,7 +269,113 @@ contract("Voting", (accounts) => {
           expect(prop2.voteCount).to.be.bignumber.equal(new BN(0));
         });
       });
-      describe("VotingSessionStarted state", () => {});
+
+      describe("VotingSessionStarted state", () => {
+        const _proposal = "new awesome propsal for voting";
+        const _proposal2 = "another one";
+        it("voter can vote only on VotingSessionStarted state", async () => {
+          // TODO dans un before propre a describe
+          // define the owner as voter
+          await ownerAddOwnerAsVoter();
+
+          //Add a voter
+          await ownerAddVoter1();
+
+          await expectRevert(VotingInstance.setVote(1, { from: _owner }), "Voting session havent started yet");
+
+          await VotingInstance.startProposalsRegistering({ from: _owner });
+          await expectRevert(VotingInstance.setVote(1, { from: _owner }), "Voting session havent started yet");
+          //Add proposals for test
+          await VotingInstance.addProposal(_proposal, { from: _voter1 });
+          await VotingInstance.addProposal(_proposal2, { from: _voter1 });
+
+          await VotingInstance.endProposalsRegistering({ from: _owner });
+          await expectRevert(VotingInstance.setVote(1, { from: _owner }), "Voting session havent started yet");
+
+          // ok
+          await VotingInstance.startVotingSession({ from: _owner });
+          let tx = await VotingInstance.setVote(1, { from: _voter1 });
+          expectEvent(tx, "Voted", { voter: _voter1, proposalId: new BN(1) });
+
+          // Not allowed on other states
+          await VotingInstance.endVotingSession({ from: _owner });
+          await expectRevert(VotingInstance.setVote(1, { from: _voter1 }), "Voting session havent started yet");
+
+          await VotingInstance.tallyVotes({ from: _owner });
+          await expectRevert(VotingInstance.setVote(1, { from: _voter1 }), "Voting session havent started yet");
+        });
+
+        it("voter can vote only once ", async () => {
+          // TODO dans un before propre a describe
+          // define the owner as voter
+          await ownerAddOwnerAsVoter();
+
+          //Add a voter
+          await ownerAddVoter1();
+
+          await expectRevert(VotingInstance.setVote(1, { from: _owner }), "Voting session havent started yet");
+
+          await VotingInstance.startProposalsRegistering({ from: _owner });
+          await expectRevert(VotingInstance.setVote(1, { from: _owner }), "Voting session havent started yet");
+          //Add proposals for test
+          await VotingInstance.addProposal(_proposal, { from: _voter1 });
+          await VotingInstance.addProposal(_proposal2, { from: _voter1 });
+
+          await VotingInstance.endProposalsRegistering({ from: _owner });
+          await expectRevert(VotingInstance.setVote(1, { from: _owner }), "Voting session havent started yet");
+
+          // ok
+          await VotingInstance.startVotingSession({ from: _owner });
+          let tx = await VotingInstance.setVote(1, { from: _voter1 });
+          expectEvent(tx, "Voted", { voter: _voter1, proposalId: new BN(1) });
+
+          //2nd time KO
+          await expectRevert(VotingInstance.setVote(1, { from: _voter1 }), "You have already voted");
+        });
+
+        it("voter vote and proposal not found ", async () => {
+          // TODO dans un before propre a describe
+          // define the owner as voter
+          await ownerAddOwnerAsVoter();
+
+          //Add a voter
+          await ownerAddVoter1();
+
+          await expectRevert(VotingInstance.setVote(1, { from: _owner }), "Voting session havent started yet");
+
+          await VotingInstance.startProposalsRegistering({ from: _owner });
+          await expectRevert(VotingInstance.setVote(1, { from: _owner }), "Voting session havent started yet");
+          //Add proposals for test
+          await VotingInstance.addProposal(_proposal, { from: _voter1 });
+          await VotingInstance.addProposal(_proposal2, { from: _voter1 });
+
+          await VotingInstance.endProposalsRegistering({ from: _owner });
+          await expectRevert(VotingInstance.setVote(1, { from: _owner }), "Voting session havent started yet");
+
+          //proposal not found
+          await VotingInstance.startVotingSession({ from: _owner });
+          //await expectRevert(VotingInstance.setVote(-1, { from: _owner }), "Proposal not found"); //marche pas -1
+          await expectRevert(VotingInstance.setVote(3, { from: _owner }), "Proposal not found"); // GENESIS bloc
+        });
+      });
+      describe("Management states event emited", () => {
+        it("check each event is emited on worflow change", async () => {
+          let tx = await VotingInstance.startProposalsRegistering({ from: _owner });
+          expectEvent(tx, "WorkflowStatusChange", { previousStatus: new BN(0), newStatus: new BN(1) });
+
+          tx = await VotingInstance.endProposalsRegistering({ from: _owner });
+          expectEvent(tx, "WorkflowStatusChange", { previousStatus: new BN(1), newStatus: new BN(2) });
+
+          tx = await VotingInstance.startVotingSession({ from: _owner });
+          expectEvent(tx, "WorkflowStatusChange", { previousStatus: new BN(2), newStatus: new BN(3) });
+
+          tx = await VotingInstance.endVotingSession({ from: _owner });
+          expectEvent(tx, "WorkflowStatusChange", { previousStatus: new BN(3), newStatus: new BN(4) });
+
+          tx = await VotingInstance.tallyVotes({ from: _owner });
+          expectEvent(tx, "WorkflowStatusChange", { previousStatus: new BN(4), newStatus: new BN(5) });
+        });
+      });
     });
 
     /**
@@ -277,3 +390,4 @@ contract("Voting", (accounts) => {
     });
   });
 });
+// oublié les events
